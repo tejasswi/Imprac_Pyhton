2a4f966abf53043ff550fe06f3af68acb7aca7c3
tejasswi <tejakommareddy1313@gmail.com>
2022-04-22 14:07:50 +0530

    renamed files

--- palindroms.py
+++ palindroms.py
@@ -2,7 +2,7 @@ import sys
 import load_dictionary
 
 
-word_list = load_dictionary.load('palingrams.txt')
+word_list = load_dictionary.load('2of12.txt')
 pali_list = []
 for word in word_list:
     if len(word)> 1 and word == word[::-1]:
