0dd89c476cc4b36d729af82531f285158c970afb
tejasswi <tejakommareddy1313@gmail.com>
2022-04-22 13:58:15 +0530

    new palindromes/usr/bin/python3.8 /home/tejaswi/Documents/tig_projects/Genrating_Pseudonymns/palingrams.py

--- palingrams.py
+++ palingrams.py
@@ -5,7 +5,7 @@ import load_dictionary
 word_list = load_dictionary.load('palingrams.txt')
 pali_list = []
 for word in word_list:
-    if len(word)> 1 and word == word[::1]:
+    if len(word)> 1 and word == word[::-1]:
         pali_list.append(word)
 print("\nNumber of Palinndormes found = {}\n".format(len(pali_list)))
 print(*pali_list,sep ='\n')
@@ -13,14 +13,14 @@ print(*pali_list,sep ='\n')
 filename = "palingrams.txt"
 x=0
 #initiation
-with open('palingrams.txt','r') as text:
+"""with open('palingrams.txt','r') as text:
     for line in text: #for loop
         line = line.split() #to split the line into words
-        for i in range(0,100):
+        for i in range(0,2):
             if line[i] == line[i][::-1]: 
                 x +=1 #increment
 
-print(x)
+print(x)"""
 
 
 
